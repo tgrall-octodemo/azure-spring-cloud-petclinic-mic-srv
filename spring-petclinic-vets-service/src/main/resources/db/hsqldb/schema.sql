edaea16db6ea1a815ef89ba068dad20bde004a0b
ezYakaEagle442 <stepin.cloud@outlook.com>
2022-04-04 19:14:20 +0200

    added missing statement CREATE DATABASE IF NOT EXISTS petclinic;

--- spring-petclinic-vets-service/src/main/resources/db/hsqldb/schema.sql
+++ spring-petclinic-vets-service/src/main/resources/db/hsqldb/schema.sql
@@ -1,3 +1,8 @@
+CREATE DATABASE IF NOT EXISTS petclinic;
+GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
+
+USE petclinic;
+
 DROP TABLE vet_specialties IF EXISTS;
 DROP TABLE vets IF EXISTS;
 DROP TABLE specialties IF EXISTS;
